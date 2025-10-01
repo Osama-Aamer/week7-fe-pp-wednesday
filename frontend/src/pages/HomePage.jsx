ad0f1eec0d89502189607172f9579deb4a8024ce
Mehdi <mehdicontacto@gmail.com>
2025-10-01 12:06:08 +0300

    Home page done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,9 @@
-import JobListings from "../components/ProductListings";
+import ProductListings from "../components/ProductListings";
 
 const Home = () => {
   return (
     <div className="home">
-      <JobListings  />
+      <ProductListings  />
     </div>
   );
 };
